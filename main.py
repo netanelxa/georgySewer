a108e59c9e324c7df9e664fc5f34b3dbd4a53184
netanelxa <58979197+netanelxa@users.noreply.github.com>
2021-05-24 11:30:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,28 @@ import sys
 
 def main():
     all_mutations = pd.read_csv("all_mutations.csv")
+    knownmuts = pd.read_csv("known_muts.csv")
+    b117muts = pd.read_csv("b117muts.csv")
     uniqueIDs=pd.unique(all_mutations["Sequence ID"])
     uniqueMuts=pd.unique(all_mutations["nuc name"])
     numofSequences=len(uniqueIDs)
     mutNucCount=all_mutations['nuc name'].value_counts()
     freqs=(mutNucCount/ numofSequences*100)
     freqs=freqs.T.to_dict()
-
     freqTable=pd.DataFrame()
     freqTable['nuc name']=uniqueMuts
     mapping = dict(all_mutations[['nuc name', 'protein']].values)
     freqTable['protein'] = freqTable['nuc name'].map(mapping)
+    mapping = dict(knownmuts[['nuc.name', 'AAMutation']].values)
+    freqTable['Known mutation'] = freqTable['nuc name'].map(mapping)
     count_title='Count ('+str(numofSequences)+')'
     freqTable[count_title]=freqTable['nuc name'].map(all_mutations['nuc name'].value_counts())
     freqTable['Freq'] = freqTable['nuc name'].map(freqs)
     freqTable.sort_values(by=['protein', 'Freq'],ascending=False,inplace=True)
     freqTable=freqTable.loc[freqTable['Freq'] >= 2]
-
+    mapping = dict(b117muts[['nucleotide', 'lineage original']].values)
+    freqTable['isUKLineage'] = freqTable['nuc name'].map(mapping)
     freqTable.to_csv("Freq_Table.csv", index=False)
-
     print("Freq_Table.csv is ready")
 
 
@@ -31,3 +34,4 @@ def main():
 if __name__ == '__main__':
     sewerannotator.main(sys.argv[1])
     main()
+
