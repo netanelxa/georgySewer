e7c087ff916738c0bc03e947d9d0c7739f1f717d
netanelxa <58979197+netanelxa@users.noreply.github.com>
2021-05-27 12:49:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
 #!/usr/bin/python
 import pandas as pd
-import covidAnnotator
+import sewerannotator
 import sys
 
-def main():
-    all_mutations = pd.read_csv("all_mutations.csv")
-    b117muts = pd.read_csv("b117muts.csv")
-    uniqueIDs=pd.unique(all_mutations["Sequence ID"])
-    uniqueMuts=pd.unique(all_mutations["nuc name"])
-    numofSequences=len(uniqueIDs)
-    mutNucCount=all_mutations['nuc name'].value_counts()
-    freqs=(mutNucCount/ numofSequences*100)
-    freqs=freqs.T.to_dict()
 
-    freqTable=pd.DataFrame()
-    freqTable['nuc name']=uniqueMuts
-    mapping = dict(all_mutations[['nuc name', 'type']].values)
-    freqTable['type'] = freqTable['nuc name'].map(mapping)
+def calculateFreqs(month):
+    all_mutations = pd.read_csv('all_mutations_' + month + '.csv')
+    knownmuts = pd.read_csv("known_muts.csv")
+    b117muts = pd.read_csv("b117muts.csv")
+    uniqueIDs = pd.unique(all_mutations["Sequence ID"])
+    uniqueMuts = pd.unique(all_mutations["nuc name"])
+    numofSequences = len(uniqueIDs)
+    mutNucCount = all_mutations['nuc name'].value_counts()
+    freqs = (mutNucCount / numofSequences * 100)
+    freqs = freqs.T.to_dict()
+    freqTable = pd.DataFrame()
+    freqTable['nuc name'] = uniqueMuts
     mapping = dict(all_mutations[['nuc name', 'protein']].values)
     freqTable['protein'] = freqTable['nuc name'].map(mapping)
-    mapping = dict(all_mutations[['nuc name', 'AAMutation']].values)
-    freqTable['AAMutation'] = freqTable['nuc name'].map(mapping)
-    count_title='Count ('+str(numofSequences)+')'
-    freqTable[count_title]=freqTable['nuc name'].map(all_mutations['nuc name'].value_counts())
+    mapping = dict(knownmuts[['nuc.name', 'AAMutation']].values)
+    freqTable['Known mutation'] = freqTable['nuc name'].map(mapping)
+    count_title = 'Count (' + str(numofSequences) + ')'
+    freqTable[count_title] = freqTable['nuc name'].map(all_mutations['nuc name'].value_counts())
     freqTable['Freq'] = freqTable['nuc name'].map(freqs)
-    freqTable.sort_values(by=['protein', 'Freq'],ascending=False,inplace=True)
-    freqTable=freqTable.loc[freqTable['Freq'] >= 2]
+    freqTable.sort_values(by=['protein', 'Freq'], ascending=False, inplace=True)
+    freqTable = freqTable.loc[freqTable['Freq'] >= 2]
     mapping = dict(b117muts[['nucleotide', 'lineage original']].values)
     freqTable['isUKLineage'] = freqTable['nuc name'].map(mapping)
     num_muts = len(freqTable.index) - 1
     if num_muts <= 0:
         num_muts = 0
     geneFreq = freqTable['protein'].value_counts()  # + " \ " + str(num_muts)
-    geneFreq['total'] = num_muts
-    geneFreq = pd.DataFrame(geneFreq)
+    geneFreq['total'] =num_muts
+    geneFreq=pd.DataFrame(geneFreq)
     if not geneFreq.empty:
         geneFreq['Freq'] = geneFreq / num_muts * 100
 
-    writer = pd.ExcelWriter("Freq_Table" + ".xlsx", engine='xlsxwriter')
+    writer = pd.ExcelWriter("Freq_Table_" + month + ".xlsx", engine='xlsxwriter')
     freqTable.to_excel(writer, sheet_name='Mutations Frequencies', index=False)
     geneFreq.to_excel(writer, sheet_name='gene count')
     writer.save()
-    print("Freq_Table" + ".csv is ready")
+
+    print("Freq_Table_" + month + ".csv is ready")
+
+
+def main():
+    months = ["env_feb_samples.txt", "env_mar_samples.txt", "env_apr_samples.txt", "env_may_samples.txt"]
+    for month in months:
+        month_str = month.split("_")[1]
+        calculateFreqs(month_str)
 
 
 if __name__ == '__main__':
-    covidAnnotator.main(sys.argv[1:])
+    sewerannotator.main(sys.argv[1])
     main()
